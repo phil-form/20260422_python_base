1bad446c948887b90716b2381780d5d4cf1c5b11
Philippe Stons <philippe.stons@bstorm.be>
2026-04-22 13:41:17 +0200

    [INIT]

---
+++ 01_Base/input_output.py
@@ -0,0 +1,9 @@
+print("Hello World")
+
+my_val = input("Please enter a number: ")
+print(my_val)
+print(type(my_val))
+
+my_int_val = int(input("Please enter a number: "))
+print(my_int_val)
+print(type(my_int_val))
